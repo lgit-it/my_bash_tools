86bf0b1dbf32336e36c44b75923e7196a6efd933
Luigi Gregori Information Technology srls <39184092+lgit-it@users.noreply.github.com>
2021-03-30 15:26:58 +0200

    create my_alias.sh

---
+++ my_alias.sh
@@ -0,0 +1,65 @@
+## Colorize the ls output ##
+alias ls='ls --color=auto'
+ 
+## Use a long listing format ##
+alias ll='ls -la'
+ 
+## Show hidden files ##
+alias l.='ls -d .* --color=auto'
+## get rid of command not found ##
+alias cd..='cd ..'
+ 
+## a quick way to get out of current directory ##
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+## Colorize the grep command output for ease of use (good for log files)##
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+#5: Create parent directories on demand
+alias mkdir='mkdir -pv'
+
+# handy short cuts #
+alias h='history'
+alias hg='history | grep'
+alias j='jobs -l'
+
+#9: Create a new set of commands
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+# Parenting changing perms on / #
+alias chown='chown --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chgrp='chgrp --preserve-root'
+
+# distro specific  - Debian / Ubuntu and friends #
+# install with apt-get
+alias apt-get="sudo apt-get"
+alias updatey="sudo apt-get --yes"
+ 
+# update on one command
+alias update='sudo apt-get update && sudo apt-get upgrade'
+
+# reboot / halt / poweroff
+alias reboot='sudo /sbin/reboot'
+alias poweroff='sudo /sbin/poweroff'
+alias halt='sudo /sbin/halt'
+alias shutdown='sudo /sbin/shutdown'
+
+## set some other defaults ##
+alias df='df -H'
+alias du='du -ch'
+ 
+# top is atop, just like vi is vim
+alias top='atop'
+
+alias mc='~/my_bash_tools/exitcwd.sh
